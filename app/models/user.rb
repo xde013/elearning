0db3d283a482cb113d32ea0899b36e82896e3c52
Ryan Bourhil <bourhilr@gmail.com>
2017-06-12 02:22:05 +0000

    Fix user sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Default Devise validation
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
+  EMAIL_REGX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # Double validation 
+  validates_format_of :email, :with => EMAIL_REGX
+  validates_uniqueness_of :email, :case_sensitive => false, :message => "Email already taken!"
+  # Associations 
   has_many :courses
+  belongs_to :university, optional: true
+  
 end
