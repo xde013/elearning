523f16d17791d6b35214f86563329d9523aadb52
Ryan Bourhil <bourhilr@gmail.com>
2017-06-13 19:31:14 +0000

    Fix Course new form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -85,7 +85,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      
-      params.require(:course).permit(:title, :description, :length, :subjectstring, :level, :image_url, :slug)
+      params.require(:course).permit(:title, :description, :length, :subjectstring, :level, :image_url, :slug, :university_id)
     end
 end
